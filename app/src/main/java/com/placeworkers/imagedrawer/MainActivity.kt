0d2eb9d62de4d8c21f13fb6d9e39f30385a16e45
radumaerza <radu.2010@gmx.de>
2020-11-09 18:46:20 +0100

    some refactoring for the ugly image converter Utils :D

--- app/src/main/java/com/placeworkers/imagedrawer/MainActivity.kt
+++ app/src/main/java/com/placeworkers/imagedrawer/MainActivity.kt
@@ -3,8 +3,8 @@ package com.placeworkers.imagedrawer
 import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.placeworkers.imageeditor.ImageConverter
-import com.placeworkers.imageeditor.ImageConverter.toBase64String
+import com.placeworkers.imageeditor.ImageConverterUtil
+import com.placeworkers.imageeditor.ImageConverterUtil.toBase64
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -13,12 +13,15 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val resourcesBitmap = BitmapFactory.decodeResource(resources, R.drawable.mountains)
-        val base64ImageString = resourcesBitmap.toBase64String()
+        val base64Image = resourcesBitmap.toBase64()
 
-        val imageFromBase64 = ImageConverter.imageFromBase64String(base64ImageString)
-        image_view.setImageBitmap(imageFromBase64)
-        // FIXME: Du kannst nicht einfach hier so ein fetten Base64 String reinklatschen in voller laenge
-        text_view.text = base64ImageString
+        val imageFromBase64 = ImageConverterUtil.imageFromBase64(base64Image)
+        requireNotNull(imageFromBase64) {
+            "Das Base64 darf nicht null sein!"
+        }
+        imageView.setImageBitmap(imageFromBase64)
+        // FIXME: Du kannst nicht einfach hier so ein fetten Base64 String reinklatschen und in voller laenge anzeigen, das kann man sowieso nicht sinnvoll anzeigen.
+        textView.text = base64Image
     }
 
 }
