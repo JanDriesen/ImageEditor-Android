daa0754f03549147cf5c948eddabeab63cc66395
Radu Märza <44721909+radumaerza@users.noreply.github.com>
2020-11-10 09:42:08 +0100

    Slow rendering fix (#1)
    * fixed rendering problems
    
    * added todos and a lovely extension function
    
    * some refactoring for the ugly image converter Utils :D
    
    Co-authored-by: radumaerza <radu.2010@gmx.de>

--- app/src/main/java/com/placeworkers/imagedrawer/MainActivity.kt
+++ app/src/main/java/com/placeworkers/imagedrawer/MainActivity.kt
@@ -1,72 +1,27 @@
 package com.placeworkers.imagedrawer
 
-import android.content.Context
-import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.AttributeSet
-import android.util.Log
-import android.view.View
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.TextView
-import com.placeworkers.imageeditor.ImageConverter
-import java.util.function.LongFunction
-import kotlin.system.measureTimeMillis
-
-const val MAIN_ACTIVITY = "MAIN_ACTIVITY"
+import com.placeworkers.imageeditor.ImageConverterUtil
+import com.placeworkers.imageeditor.ImageConverterUtil.toBase64
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
-    // preview for the image / bitmap
-    private lateinit var imageView: ImageView
-
-     // preview for the base64 String
-    private lateinit var textView: TextView
-
-    private var originalBitmap: Bitmap? = null
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        imageView = findViewById(R.id.image_view)
-        textView = findViewById(R.id.text_view)
+        val resourcesBitmap = BitmapFactory.decodeResource(resources, R.drawable.mountains)
+        val base64Image = resourcesBitmap.toBase64()
 
-        // load the BitMap from the resources
-        val decodeTime = measureTimeMillis {
-            originalBitmap = BitmapFactory.decodeResource(resources, R.drawable.mountains)
+        val imageFromBase64 = ImageConverterUtil.imageFromBase64(base64Image)
+        requireNotNull(imageFromBase64) {
+            "Das Base64 darf nicht null sein!"
         }
-        Log.d(MAIN_ACTIVITY, "Decoding Image from Resources has taken $decodeTime milliseconds (${decodeTime / 1000} seconds)")
-        Log.d(MAIN_ACTIVITY, "Finished decoding from Resources")
-
-        // measure the time of image to string
-        val imageToBase64Time = measureTimeMillis {
-            originalBitmap?.let {
-                ImageConverter.base64StringFromImage(it).let { base64 ->
-                    textView.text = base64
-                }
-            }
-        }
-        Log.d(MAIN_ACTIVITY, "Converting Image To base64 with Size of Height: ${originalBitmap?.height} Width: ${originalBitmap?.width} takes: $imageToBase64Time milliseconds (${imageToBase64Time / 1000} seconds)")
-        Log.d(MAIN_ACTIVITY, "Finished converting to string")
-
-        // measure the time of string to image
-        val base64ToImageTime = measureTimeMillis {
-            textView.text?.let { text ->
-                ImageConverter.imageFromBase64String(text as String)?.let { convertedBitmap ->
-                    imageView.setImageBitmap(convertedBitmap)
-                }
-            }
-        }
-        Log.d(MAIN_ACTIVITY, "Converting from base64 to Image with a size of Height: ${originalBitmap?.height} Width: ${originalBitmap?.width} takes: $base64ToImageTime milliseconds (${base64ToImageTime / 1000} seconds)")
-        Log.d(MAIN_ACTIVITY, "Finished converting to image")
-
-    }
-
-    override fun onStart() {
-        super.onStart()
+        imageView.setImageBitmap(imageFromBase64)
+        // FIXME: Du kannst nicht einfach hier so ein fetten Base64 String reinklatschen und in voller laenge anzeigen, das kann man sowieso nicht sinnvoll anzeigen.
+        textView.text = base64Image
     }
 
 }
